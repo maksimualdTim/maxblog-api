6841a98d5819ea3cf0be601864b34bfc9f7d4e6d
maksimus <timofeev2001maksim@gmail.com>
2021-11-05 20:01:48 +0300

    fix database factories & migration logic

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Article;
 use App\Models\Category;
 use App\Models\File;
 use App\Models\User;
@@ -17,19 +18,50 @@ class ArticleFactory extends Factory
     public function definition()
     {
         return [
-            'author_id' => function () {
-                return User::factory()->create()->id;
-            },
-            'title' => $this->faker->title,
+            'author_id' => $this->foreignKeyRandomItem(User::class),
+            'title' => $this->faker->asciify('********************'),
             'shortText' => $this->faker->text(500),
             'text' => $this->faker->text(1000),
-            'preview' => function () {
-                return File::factory()->create()->id;
-            },
             'views' => $this->faker->numberBetween(0, 200),
-            'category' => function () {
-                return Category::factory()->create()->id;
-            },
+            'category_id' => $this->foreignKeyRandomItem(Category::class),
         ];
     }
+
+    private function foreignKeyRandomItem($class)
+    {
+        $object = $class::all();
+        if($object->count() !== 0){
+            return rand(1, $object->count());
+        }else{
+            return $class::factory()->create()->id;
+        }
+    }
+
+    /**
+     * Configure the model factory.
+     *
+     * @return $this
+     */
+    public function configure()
+    {
+        return $this->afterMaking(function (Article $article) {
+            //
+        })->afterCreating(function (Article $article) {
+            $files = File::all();
+
+            if($files->count() !== 0){
+                $id = rand(1, $files->count());
+                $file = File::where('id', '=', $id)->where('article_id', '=', null);
+
+                if($file->count() !== 0){
+                    $article->update(['preview' => $id]);
+                    $file->update(['article_id' => $article->id]);
+
+                    $file::save();
+                    $article::save();
+                }
+            }
+        });
+    }
+
 }
--- database/factories/FileFactory.php
+++ database/factories/FileFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Article;
+use App\Models\File;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -15,13 +17,42 @@ class FileFactory extends Factory
     public function definition()
     {
         return [
-            'user_id' => function () {
-                return User::factory()->create()->id;
-            },
-            'filename' => $this->faker->firefox,
+            'user_id' => $this->foreignKeyRandomItem(User::class),
+            'filename' => $this->faker->asciify('********************'),
             'file_type' => 'jpg',
             'content_type' => 'image/jpeg',
             'byte_size' => $this->faker->randomNumber(),
         ];
     }
+
+    private function foreignKeyRandomItem($class)
+    {
+        $object = $class::all();
+        if($object->count() !== 0){
+            return rand(1, $object->count());
+        }else{
+            return $class::factory()->create()->id;
+        }
+    }
+
+    /**
+     * Configure the model factory.
+     *
+     * @return $this
+     */
+    public function configure()
+    {
+        return $this->afterMaking(function (File $file) {
+            //
+        })->afterCreating(function (File $file) {
+            $articles = Article::all();
+
+            if($articles->count() !== 0){
+                $id = rand(1, $articles->count());
+                $file->update(['article_id' => $id]);
+                $file->save();
+            }
+        });
+    }
 }
+
